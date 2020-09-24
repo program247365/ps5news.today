33516911d97731dbde4e3b23ebb0491e0dfb1d0f
Kevin B. Ridgway <kevin@interweblabs.com>
2020-09-24 08:57:48 -0400

    styling updates

--- pages/index.js
+++ pages/index.js
@@ -69,9 +69,8 @@ export default function IndexPage({ articles }) {
 function Article({ title, url, date, time, image, excerpt, tags }) {
   const largeImageUrl = image[0]?.thumbnails?.large?.url; // TODO: fallback image for not one here
   return (
-    <div className="bg-white border rounded-lg overflow-hidden sm:w-full sm:m-1 md:px-2 md:w-1/3 lg:px-0 lg:w-1/4 lg:m-1 xl:my-2 xl:m-1 xl:px-5">
-    <div className="h-48 bg-cover bg-center" style={{ backgroundImage: `url(${largeImageUrl})` }}>
-    </div>
+    <div className="relative bg-white border rounded-lg overflow-hidden sm:w-full sm:m-1 md:px-2 md:w-1/3 lg:px-0 lg:w-1/4 lg:m-1 xl:my-2 xl:m-1 xl:pb-4">
+    <div className="h-48 bg-cover bg-center" style={{ backgroundImage: `url(${largeImageUrl})` }}></div>
       <div className="p-6">
         <h4 className="font-semibold text-lg leading-tight"><a href={url} target="_blank" rel="noopener">{title}</a></h4>
         <div className="mt-1">
@@ -81,7 +80,7 @@ function Article({ title, url, date, time, image, excerpt, tags }) {
           <span className="text-gray-600 text-sm">{excerpt}</span>
         </div>
       </div>
-      <span className="text-white text-sm bg-yellow-400 rounded-lg p-1">{tags}</span>
+      <span className="text-black-100 font-semibold text-sm bg-yellow-400 sm:p-2 rounded-md p-1 xl:px-4 xl:mx-3 absolute top-0 right-0 m-3">{tags}</span>
     </div>
   );
 }
