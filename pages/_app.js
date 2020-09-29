abe40a419faea3a7e934321495ebde54203f4847
Kevin B. Ridgway <kevin@interweblabs.com>
2020-09-28 23:34:36 -0400

    Adding Fathom analytics.

--- pages/_app.js
+++ pages/_app.js
@@ -1,25 +1,32 @@
-import React, { useEffect } from 'react'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 import SEO from '../next-seo.config'
 import { NextSeo } from 'next-seo'
 import '../styles/index.css'
 import Head from '../components/siteHead'
-import Router from 'next/router'
 import * as Fathom from 'fathom-client'
-
-// Record a pageview when route changes
-Router.events.on('routeChangeComplete', () => {
-  Fathom.trackPageview();
-});
 
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
+    const router = useRouter()
     const FATHOM_TRACKING_JS_URL = '//stats.interweblabs.app/tracker.js'
     // Initialize Fathom when the app loads
     Fathom.load('XYAEY', {
+      includedDomains: ['ps5news.today'],
       excludedDomains: ["vercel.app", "now.sh", "localhost"],
       url: FATHOM_TRACKING_JS_URL,
-      honorDNT: true,
     })
+
+    function onRouteChangeComplete() {
+      Fathom.trackPageview()
+    }
+    // Record a pageview when route changes
+    router.events.on('routeChangeComplete', onRouteChangeComplete)
+
+    // Unassign event listener
+    return () => {
+      router.events.off('routeChangeComplete', onRouteChangeComplete)
+    }
   }, [])
 
   return (
