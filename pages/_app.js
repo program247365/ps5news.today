fd15855adf8472041f58c2f39e8cc13c078c188d
Kevin B. Ridgway <kevin@interweblabs.com>
2020-09-28 23:01:48 -0400

    Adding Fathom analytics.

--- pages/_app.js
+++ pages/_app.js
@@ -10,27 +10,25 @@ function MyApp({ Component, pageProps }) {
   const router = useRouter()
 
   useEffect(() => {
-    if (typeof window.fathom === 'undefined') {
-      const FATHOM_INCLUDE_URL = 'ps5news.today'
-      const FATHOM_TRACKING_JS_URL = '//stats.interweblabs.app/tracker.js'
-      // Initialize Fathom when the app loads
-      Fathom.load('XYAEY', {
-        includedDomains: [FATHOM_INCLUDE_URL],
-        excludedDomains: ['localhost:3000, localhost:3001, *.vercel.app'],
-        url: FATHOM_TRACKING_JS_URL,
-        honorDNT: true,
-      })
+    const FATHOM_INCLUDE_URL = 'ps5news.today'
+    const FATHOM_TRACKING_JS_URL = '//stats.interweblabs.app/tracker.js'
+    // Initialize Fathom when the app loads
+    Fathom.load('XYAEY', {
+      includedDomains: [FATHOM_INCLUDE_URL],
+      excludedDomains: ['localhost:3000, localhost:3001, *.vercel.app'],
+      url: FATHOM_TRACKING_JS_URL,
+      honorDNT: true,
+    })
 
-      function onRouteChangeComplete() {
-        Fathom.trackPageview()
-      }
-      // Record a pageview when route changes
-      router.events.on('routeChangeComplete', onRouteChangeComplete)
+    function onRouteChangeComplete() {
+      Fathom.trackPageview()
+    }
+    // Record a pageview when route changes
+    router.events.on('routeChangeComplete', onRouteChangeComplete)
 
-      // Unassign event listener
-      return () => {
-        router.events.off('routeChangeComplete', onRouteChangeComplete)
-      }
+    // Unassign event listener
+    return () => {
+      router.events.off('routeChangeComplete', onRouteChangeComplete)
     }
   }, [])
 
