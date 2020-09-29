40df694a2f6332ab04a753356765edd7c1e9c70f
Kevin B. Ridgway <kevin@interweblabs.com>
2020-09-28 23:11:14 -0400

    Adding Fathom analytics.

--- pages/_app.js
+++ pages/_app.js
@@ -3,35 +3,24 @@ import SEO from '../next-seo.config'
 import { NextSeo } from 'next-seo'
 import '../styles/index.css'
 import Head from '../components/siteHead'
-import { useRouter } from 'next/router'
 import * as Fathom from 'fathom-client'
 
+// Record a pageview when route changes
+Router.events.on('routeChangeComplete', () => {
+  Fathom.trackPageview();
+});
+
 function MyApp({ Component, pageProps }) {
-  const router = useRouter()
-
   useEffect(() => {
-    if (typeof window.fathom === 'undefined') {
-      const FATHOM_INCLUDE_URL = 'ps5news.today'
-      const FATHOM_TRACKING_JS_URL = '//stats.interweblabs.app/tracker.js'
-      // Initialize Fathom when the app loads
-      Fathom.load('XYAEY', {
-        includedDomains: [FATHOM_INCLUDE_URL],
-        excludedDomains: ['localhost:3000, localhost:3001, *.vercel.app'],
-        url: FATHOM_TRACKING_JS_URL,
-        honorDNT: true,
-      })
-
-      function onRouteChangeComplete() {
-        Fathom.trackPageview()
-      }
-      // Record a pageview when route changes
-      router.events.on('routeChangeComplete', onRouteChangeComplete)
-
-      // Unassign event listener
-      return () => {
-        router.events.off('routeChangeComplete', onRouteChangeComplete)
-      }
-    }
+    const FATHOM_INCLUDE_URL = 'ps5news.today'
+    const FATHOM_TRACKING_JS_URL = '//stats.interweblabs.app/tracker.js'
+    // Initialize Fathom when the app loads
+    Fathom.load('XYAEY', {
+      includedDomains: [FATHOM_INCLUDE_URL],
+      excludedDomains: ['localhost:3000, localhost:3001, *.vercel.app'],
+      url: FATHOM_TRACKING_JS_URL,
+      honorDNT: true,
+    })
   }, [])
 
   return (
