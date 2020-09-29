344a8efbb633169b4d01402415bc19b1c64cb482
Kevin B. Ridgway <kevin@interweblabs.com>
2020-09-28 22:27:12 -0400

    Add privacy-based tracking.

--- pages/_app.js
+++ pages/_app.js
@@ -4,11 +4,32 @@ import '../styles/index.css'
 import Head from '../components/siteHead'
 
 function MyApp({ Component, pageProps }) {
+  const [isProd, setisProd] = React.useState(false);
+  React.useEffect(() => {
+    const isProd = window?.location?.host.split(':')[0] === 'ps5news.today';
+  }, []);
+
   return (
     <>
       <NextSeo {...SEO}/>
       <Head />
       <Component {...pageProps} />
+      {/*<!-- Fathom - simple website analytics - https://github.com/usefathom/fathom --> */}
+      { isProd && <script dangerouslySetInnerHTML={{
+        __html: `
+      (function(f, a, t, h, o, m){
+        a[h]=a[h]||function(){
+          (a[h].q=a[h].q||[]).push(arguments)
+        };
+        o=f.createElement('script'),
+        m=f.getElementsByTagName('script')[0];
+        o.async=1; o.src=t; o.id='fathom-script';
+        m.parentNode.insertBefore(o,m)
+      })(document, window, '//stats.interweblabs.app/tracker.js', 'fathom');
+      fathom('set', 'siteId', 'XYAEY');
+      fathom('trackPageview');
+      `}} /> }
+      {/* <!-- / Fathom --> */}
     </>
   )
 }
