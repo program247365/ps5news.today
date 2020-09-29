7b89427e46c3b008b78afce2d64b02fc479fa675
Kevin B. Ridgway <kevin@interweblabs.com>
2020-09-28 22:46:56 -0400

    Proper fathom analytics added.

--- pages/_app.js
+++ pages/_app.js
@@ -1,36 +1,42 @@
+import { useEffect } from 'react'
 import SEO from '../next-seo.config'
 import { NextSeo } from 'next-seo'
 import '../styles/index.css'
 import Head from '../components/siteHead'
+import { useRouter } from 'next/router'
+import * as Fathom from 'fathom-client'
 
 function MyApp({ Component, pageProps }) {
-  const [isProd, setisProd] = React.useState(false);
-  React.useEffect(() => {
-    const isProd = window?.location?.host === 'ps5news.today';
-    console.log('isProd', isProd);
-  }, []);
+  const router = useRouter()
+
+  useEffect(() => {
+    const FATHOM_INCLUDE_URL = 'ps5news.today'
+    const FATHOM_TRACKING_JS_URL = '//stats.interweblabs.app/tracker.js'
+    // Initialize Fathom when the app loads
+    Fathom.load('XYAEY', {
+      includedDomains: [FATHOM_INCLUDE_URL],
+      excludedDomains: ['localhost:3000, localhost:3001, *.vercel.app'],
+      url: FATHOM_TRACKING_JS_URL,
+      honorDNT: true,
+    })
+
+    function onRouteChangeComplete() {
+      Fathom.trackPageview()
+    }
+    // Record a pageview when route changes
+    router.events.on('routeChangeComplete', onRouteChangeComplete)
+
+    // Unassign event listener
+    return () => {
+      router.events.off('routeChangeComplete', onRouteChangeComplete)
+    }
+  }, [])
 
   return (
     <>
       <NextSeo {...SEO}/>
       <Head />
       <Component {...pageProps} />
-      {/*<!-- Fathom - simple website analytics - https://github.com/usefathom/fathom --> */}
-      { isProd && <script dangerouslySetInnerHTML={{
-        __html: `
-      (function(f, a, t, h, o, m){
-        a[h]=a[h]||function(){
-          (a[h].q=a[h].q||[]).push(arguments)
-        };
-        o=f.createElement('script'),
-        m=f.getElementsByTagName('script')[0];
-        o.async=1; o.src=t; o.id='fathom-script';
-        m.parentNode.insertBefore(o,m)
-      })(document, window, '//stats.interweblabs.app/tracker.js', 'fathom');
-      fathom('set', 'siteId', 'XYAEY');
-      fathom('trackPageview');
-      `}} /> }
-      {/* <!-- / Fathom --> */}
     </>
   )
 }
