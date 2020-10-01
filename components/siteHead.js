20367b9f2e0273f6539a161af0952fc9735f73fd
Kevin B. Ridgway <kevin@interweblabs.com>
2020-09-30 23:04:25 -0400

    Adding fathom the only way I know how.

--- components/siteHead.js
+++ components/siteHead.js
@@ -1,5 +1,7 @@
 import Head from 'next/head'
 
+const isProd = (typeof window !== 'undefined' && window.location.host === 'ps5news.today');
+
 export default function SiteHead() {
     return (
         <Head>
@@ -11,6 +13,20 @@ export default function SiteHead() {
             <meta name="msapplication-TileColor" content="#da532c"/>
             <meta name="msapplication-config" content="/images/favicons/browserconfig.xml"/>
             <meta name="theme-color" content="#ffffff"/>
+            { isProd && <script dangerouslySetInnerHTML={{
+                __html: `
+                (function(f, a, t, h, o, m){
+                    a[h]=a[h]||function(){
+                        (a[h].q=a[h].q||[]).push(arguments)
+                    };
+                    o=f.createElement('script'),
+                    m=f.getElementsByTagName('script')[0];
+                    o.async=1; o.src=t; o.id='fathom-script';
+                    m.parentNode.insertBefore(o,m)
+                })(document, window, '//stats.interweblabs.app/tracker.js', 'fathom');
+                fathom('set', 'siteId', 'XYAEY');
+                fathom('trackPageview');
+              `}} /> }
             <script data-name="BMC-Widget" src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js" data-id="program247365" data-description="Support me on Buy me a coffee!" data-message="Like this site? Consider supporting me to keep adding great content." data-color="#79D6B5" data-position="" data-x_margin="18" data-y_margin="18"></script>
         </Head>
   );
--- pages/_app.js
+++ pages/_app.js
@@ -1,32 +1,9 @@
-import { useEffect } from 'react'
-import { useRouter } from 'next/router'
 import SEO from '../next-seo.config'
 import { NextSeo } from 'next-seo'
 import '../styles/index.css'
 import Head from '../components/siteHead'
-import * as Fathom from 'fathom-client'
 
 function MyApp({ Component, pageProps }) {
-  const router = useRouter()
-
-  useEffect(() => {
-    // Initialize Fathom when the app loads
-    Fathom.load('XYAEY', {
-      includedDomains: ['ps5news.today']
-    })
-
-    function onRouteChangeComplete() {
-      Fathom.trackPageview()
-    }
-    // Record a pageview when route changes
-    router.events.on('routeChangeComplete', onRouteChangeComplete)
-
-    // Unassign event listener
-    return () => {
-      router.events.off('routeChangeComplete', onRouteChangeComplete)
-    }
-  }, [])
-
   return (
     <>
       <NextSeo {...SEO}/>
